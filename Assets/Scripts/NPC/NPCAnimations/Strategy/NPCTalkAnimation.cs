9a35e2f3fb4e4e562ecacd9774530314f599b473
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-05-09 18:59:46 +0200

    feat: feature done and clean not used scripts and gameobjects

--- Assets/Scripts/NPC/NPCAnimations/Strategy/NPCTalkAnimation.cs
+++
@@ -1,9 +0,0 @@
-namespace NPC.NPCAnimations.Strategy
-{
-    
-    class NPCTalkAnimation : INPCAnimationStrategy
-    {
-        public void PlayAnimation(NPCAnimationManager manager) => manager.PlayAnimation(NPCAnimationsType.Talk, true);
-        public void StopAnimation(NPCAnimationManager manager) => manager.PlayAnimation(NPCAnimationsType.Idle, true);
-    }
-}
--- Assets/Scripts/TimeSystem/ITimeRewindable.cs
+++
@@ -1,9 +0,0 @@
-
-public interface ITimeRewindable
-{
-    public void InitializeStateRecording(float interval, bool adaptiveRecording);
-    public void TruncateHistoryAfter(float timePoint);
-    public void RecordState(float time);
-    public void RewindToTime(float targetTime);
-    public void ClearStates();
-}
